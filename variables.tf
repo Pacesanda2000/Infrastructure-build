724b672820586693a486b9515046990a52d59ded
David Majoros <david.majoros@t-systems.com>
2024-01-22 22:16:38 +0000

    Add new file

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vsphere_user" {
+  type        = string
+  default     = "dmajoros"
+  description = "username"
+}
+
+variable "vsphere_password" {
+  type        = string
+  default     = "Pacesanda2000."
+  description = "password"
+}
+
+variable "vsphere_server" {
+  type        = string
+  default     = "vlab.dual.edu"
+  description = "server"
+}
+
+variable "vm_name" {
+  description = "VM Names"
+  default     = "serverik-"
+  type        = string
+}
+
+variable "vm_count" {
+  description = "Number of Virtual Machines"
+  default     = 3
+  type        = string
+}
