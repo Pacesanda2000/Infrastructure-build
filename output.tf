a6429a356b88dfff4c93e47af4aa7828636754bc
David Majoros <david.majoros@t-systems.com>
2024-01-24 14:20:29 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "master_ip_addresses" {
   value = [
-    for vm in vsphere_virtual_machine.master${count.index + 1}
+    for vm in vsphere_virtual_machine.master${count.index + 1} : vm.name
   ]
 }
 
 output "worker_ip_addresses" {
   value = [
-    for vm in vsphere_virtual_machine.worker${count.index + 1}
+    for vm in vsphere_virtual_machine.worker${count.index + 1} : vm.name
   ]
 }
