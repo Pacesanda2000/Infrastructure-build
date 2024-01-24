6fd58a602c434ecaeccea3ddd18f966cc702dfaf
David Majoros <david.majoros@t-systems.com>
2024-01-24 08:43:07 +0000

    Update master.tf

--- master.tf
+++ master.tf
@@ -4,8 +4,8 @@ terraform {
       source = "hashicorp/vsphere"
     }
   }
-  #required_version = ">= 0.13"
-  required_version = "1.0.0"
+  required_version = ">= 0.13"
+  #required_version = "1.0.0"
   backend "local" {
     path = "terraform.tfstate"
   }
@@ -64,9 +64,7 @@ resource "vsphere_virtual_machine" "master" {
   resource_pool_id = data.vsphere_resource_pool.pool.id
   guest_id         = data.vsphere_virtual_machine.template.guest_id
   scsi_type        = data.vsphere_virtual_machine.template.scsi_type
-  #wait_for_guest_net_timeout = 0
-  wait_for_guest_net_routable = false
-  wait_for_guest_net_timeout = 0
+  wait_for_guest_net_timeout = -1
   wait_for_guest_ip_timeout  = 0
 
 
