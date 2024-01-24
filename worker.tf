53b6f1c4fac7e9ca5ca0421b256ed67479b5dc47
David Majoros <david.majoros@t-systems.com>
2024-01-24 09:55:57 +0000

    Update worker.tf

--- worker.tf
+++ worker.tf
@@ -13,7 +13,7 @@ resource "vsphere_virtual_machine" "worker" {
   scsi_type        = data.vsphere_virtual_machine.template.scsi_type
   #wait_for_guest_net_timeout = 0
   #wait_for_guest_net_routable = false
-  wait_for_guest_net_timeout = 0
+  wait_for_guest_net_timeout = -10
   wait_for_guest_ip_timeout  = 0
 
 
