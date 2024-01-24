a3ebddb97bc8ea94bee75c86a7a508eafd9280ff
David Majoros <david.majoros@t-systems.com>
2024-01-24 21:49:06 +0000

    Update master.tf

--- master.tf
+++ master.tf
@@ -90,7 +90,7 @@ resource "vsphere_virtual_machine" "master" {
         domain    = "vlab.dual.edu"
       }
       network_interface {
-        ipv4_address  = "10.207.0.400"
+        ipv4_address  = "10.207.0.300"
         #ipv4_address  = "10.207.0.${401 + count.index}"
         ipv4_netmask  = 24
         #dns_server_list = ["172.29.0.101", "172.29.0.103"]
