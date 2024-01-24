0e02d75194b25f758aa5e69ec0149d0b795a22ca
David Majoros <david.majoros@t-systems.com>
2024-01-24 07:54:37 +0000

    Update master.tf

--- master.tf
+++ master.tf
@@ -6,7 +6,7 @@ terraform {
   }
   required_version = ">= 0.13"
   backend "local" {
-    path = "/home/gitlab-runner/builds/zvN3A5z1/0/dualedu/2022-2023/sis/dmajoros/projekt/terraform.tfstate"
+    path = "terraform.tfstate"
   }
 }
 
